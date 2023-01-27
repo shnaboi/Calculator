fc70b908c82e24313f5770e9ac42366b60c2df6c
shnaboi <shane.karner@gmail.com>
2023-01-27 13:15:48 -0500

    Add color code to buttons. Add clear button functionality

--- script.js
+++ script.js
@@ -2,21 +2,36 @@ const screenTop = document.querySelector(".screen-top");
 const screenBottom = document.querySelector(".screen-bottom")
 const numberButtons = document.querySelectorAll(".number");
 const mdasButtons = document.querySelectorAll(".mdas")
+const deleteKey = document.querySelector(".delete")
+const clearKey = document.querySelector(".clear")
 
 numberButtons.forEach((button) => 
     button.addEventListener('click', () => appendNumber(button.textContent))
 );
 mdasButtons.forEach((button) => 
-    button.addEventListener('click', () => operation(button.textContent))
+    button.addEventListener('click', () => console.log('works'))
 );
+
+deleteKey.addEventListener('click', () => backspace());
+clearKey.addEventListener('click', () => clear());
 
 let inputOne = '';
 let inputTwo = '';
 
 // ADD APPEND NUMBER FUNCTION
 function appendNumber(x) {
-    screenBottom.textContent = x;
+    screenBottom.textContent += x;
 }
+
+function clear() {
+    screenBottom.textContent = '';
+}
+
+// function backspace() {
+//     screenBottom.textContent.slice(0, -1);
+// }
+
+console.log(screenBottom.textContent[screenBottom.textContent.length - 1])
 
 // EDIT OPERATION FUNCTION TO CHANGE OPERATION
 function operation(op) {
