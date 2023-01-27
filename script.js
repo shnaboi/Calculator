2758e37c07e9165eadb0996e6ead553c78a4bf9c
shnaboi <shane.karner@gmail.com>
2023-01-27 01:17:17 -0500

    Add Clear & Backspace buttons

--- script.js
+++ script.js
@@ -1,54 +1,23 @@
+let inputOne = '';
+let inputTwo = '';
+
+
+
 const add = function(x, y) {
-	return (x+y);
+	return x + y;
 };
 
 const subtract = function(x, y) {
-	if (x > y) {
-    return (x-y);
-  } else {
-    return (y-x);
-  }
+    return x - y;
 };
 
-const sum = function(x) {
-  let result = 0;
-  for (let i = 0; i < x.length; i++) {
-    result += x[i];
-  }
-  return result;
-};
+const multiply = function(x, y) {
+    return x * y;
+}
 
-const multiply = function(x) {
-  let result = 1;
-  for (let i = 0; i < x.length; i++) {
-    result *= x[i];
-  }
-  return result;
-};
+const divide = function(x, y) {
+    return x / y;
+}
 
-const power = function(x, y) {
-	let result = 1;
-  if (y == 0) {
-    result = 1;
-  } else if (y == 1) {
-    result = x;
-  } else {
-    for (let i = 0; i < y; i++) {
-      result *= x;
-    }
-  }
-  return result;
-};
-
-const factorial = function(x) {
-	let result = x;
-  if (x <= 1) {
-    result = 1;
-  } else {
-    for (let i = x - 1; i >= 1; i--) {
-      result *= i;
-    }
-  }
-  return result;
-};
-
+// backspace function
+// Clear function
