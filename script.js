593c82ea1ac968308f219d9862b42e7a672de993
shnaboi <shane.karner@gmail.com>
2023-01-27 01:42:17 -0500

    Add number button click functionality working in console

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
+const screen = document.querySelector("screen");
+const numberButtons = document.querySelectorAll(".number");
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => console.log('works'))
+);
+
 let inputOne = '';
 let inputTwo = '';
 
-
+function operate(op, x, y) {
+    if (op === '+') {
+        return add(x, y)
+    }
+    if (op === '-') {
+        return subtract(x, y)
+    }
+    if (op === 'x') {
+        return multiply(x, y)
+    }
+    if (op === '÷') {
+        return divide(x, y)
+    }
+}
 
 const add = function(x, y) {
 	return x + y;
